e98a2556b03c4a61423a09fffabbac7a58ed2c76
KROHNICK <business.kanekperez@gmail.com>
2019-03-08 10:15:02 -0800

    get games endpoint tests passed

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const helmet = require("helmet");
-const Cohorts = require("../database/models/games/gamesModel");
+const Games = require("../database/models/games/gamesModel");
 
 server.use(express.json());
 server.use(cors());
@@ -15,9 +15,9 @@ server.get("/", (req, res) => {
 server.get("/games", async (req, res) => {
   try {
     const games = await Games.getAll();
-    return res.status(200).json(games);
+    res.status(200).json(games);
   } catch (error) {
-    res.status(500).json({ error: "Internal error..." });
+    res.status(500).json({ error: "Internal error." });
   }
 });
 
--- api/server.spec.js
+++ api/server.spec.js
@@ -22,15 +22,24 @@ describe("server.js", () => {
   });
 
   describe("GET /games", () => {
-    it("responds with 200 OK status", async () => {
+    it("should return status code 200", async () => {
       const res = await request(server).get("/games");
       expect(res.status).toBe(200);
     });
 
-    it("responds with an array of all games in our database", async () => {
-      const cohorts = await Games.getAll();
+    it("should return json", async () => {
       const res = await request(server).get("/games");
-      expect(res.body).toHaveLength(games.length);
+      expect(typeof res.body).toEqual("object");
+    });
+
+    it("should return all games records", async () => {
+      const gameBody = {
+        title: "Chess",
+        genre: "Strategy",
+        releaseYear: 500
+      };
+      const res = await request(server).get("/games");
+      expect(res.body).toHaveLength(10);
     });
   });
 });
